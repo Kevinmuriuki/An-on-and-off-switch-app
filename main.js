ea3808066bb5bd3eb429ac17b5c1abfb95a22b62
kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2020-06-20 21:17:06 +0300

    This commit is an application that switches on and off a bulb using JavaScript

---
+++ main.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('input');
+
+input.addEventListener('click', () => {
+  if(input.checked) {
+    const image = document.getElementById('image');
+  const content = '<img src="images/pic_bulbon.gif" alt="">';
+  image.innerHTML= content;
+  } else {
+    const image = document.getElementById('image');
+  const content = '<img src="images/pic_bulboff.gif" alt="">';
+  image.innerHTML= content;
+  }
+})
